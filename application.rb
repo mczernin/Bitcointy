ff64e716bde16e4b14db65dc0befb9a5963bc193
Pablo Merino <pablo.perso1995@gmail.com>
2013-10-05 11:55:09 +0200

    Added some Geo Magic #2

--- application.rb
+++ application.rb
@@ -30,11 +30,10 @@ end
 
 get '/' do
   require 'geocoder'
-  require 'socket'
-  ip = Socket.ip_address_list.detect{|intf| intf.ipv4_private?}
-  ip.ip_address if ip
+  require 'open-uri'
+  remote_ip = open('http://whatismyip.akamai.com').read
   @ip = Geocoder.search(request.ip)
-  @public_ip = Geocoder.search(ip.ip_address) 
+  @public_ip = Geocoder.search(remote_ip) 
   error_codes = ["ERR_SERVER_GONE_BANANAS", "ERR_DONT_CARE", "ERR_SYSTEM_FAILURE", "ERR_PROJECT_X_FAIL", "ERR_DOGS_CHEWING_MODEM", "ERR_NOPE", "ERR_LINUX_AINT_UNIX", "ERR_LS_NOT_FOUND", "ERR_GOVT_SHUTDOWN"]
   
 	@random_error_code = error_codes.sample
